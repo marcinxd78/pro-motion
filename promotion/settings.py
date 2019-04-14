58bd026607b2ccb7693a92a4e293bb6ad8134810
Jimmy <stanislaw5000@o2.pl>
2019-04-14 19:14:01 +0200

    'nezt'

--- promotion/settings.py
+++ promotion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://pro-motion.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pro-motion.herokuapp.com', 'localhost']
 DEFAULT_CHARSET = 'utf-8'
 
 # Application definition
