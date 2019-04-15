fcab2b26b0f67cd5ef618bd5caeb3850dbdaa89b
Jimmy <stanislaw5000@o2.pl>
2019-04-15 19:56:50 +0200

    'z_static'

--- promotion/settings.py
+++ promotion/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'my_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/main/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
