4bcb7e35b73ec375216cb48e8f06bb8f7c0fa95b
Jimmy <stanislaw5000@o2.pl>
2019-05-04 01:51:41 +0200

    VPS1

--- promotion/settings.py
+++ promotion/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['pro-motion.herokuapp.com', 'localhost']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['46.41.151.91', 'pro-motion.herokuapp.com', 'localhost']
 DEFAULT_CHARSET = 'utf-8'
 
 # Application definition
