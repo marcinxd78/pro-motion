571b1384a31a0683946f7b44e5e455c137a0a015
Jimmy <stanislaw5000@o2.pl>
2019-05-05 01:05:59 +0200

    First_commit3

--- promotion/settings.py
+++ promotion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['dailypromotion.pl', '46.41.151.91', 'pro-motion.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['dailypromotion.pl', 'localhost']
 DEFAULT_CHARSET = 'utf-8'
 
 # Application definition
