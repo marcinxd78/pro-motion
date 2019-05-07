1fc959d477fe7d0aee026bcb4a99a7f51fef9925
Jimmy <stanislaw5000@o2.pl>
2019-05-07 21:41:15 +0200

    Change_base

--- promotion/settings.py
+++ promotion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['dailypromotion.pl', 'localhost']
+ALLOWED_HOSTS = ['www.dailypromotion.pl', 'dailypromotion.pl', 'localhost']
 DEFAULT_CHARSET = 'utf-8'
 
 # Application definition
